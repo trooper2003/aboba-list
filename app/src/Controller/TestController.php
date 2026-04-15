c4d3a6231be6654ede86625f474662bb1eb1756e
AleksandrB <alexbyk36@gmail.com>
2026-04-15 12:25:12 +0700

    added link to aboba

--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Aboba;
+use App\Repository\AbobaRepository;
 use App\Repository\TestTableRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,17 +20,39 @@ class TestController extends AbstractController
     }
 
     #[Route('/api/test', name: 'test', methods: ['GET'])]
-    public function index(Request $request): Response
+        public function index(Request $request, AbobaRepository $aboba): Response
     {
-        $records = $this->testTableRepository->findAll();
+    //        $records = $this->testTableRepository->findAll();
 
-        return $this->json(['records' => $records], Response::HTTP_OK);
-    }
+    //        return $this->json(['records' => $records], Response::HTTP_OK);
+
+            return $this->json($aboba->getAll());
+
+        }
 
     #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function appHomePage(Request $request): Response
+    public function appHomePage(Request $request, AbobaRepository $aboba): Response
     {
-        $aboba = 'Квазибоба';
-        return $this->render('app/home_page.html.twig', ['aboba' => $aboba]);
+        $abobaArray = $aboba->getAll();
+        return $this->render('app/home_page.html.twig', ['aboba' => $abobaArray]);
+    }
+
+    #[Route('/api/aboba/{id<\d+>}', name: 'app_aboba_get', methods: ['GET'])]
+    public function getAboba(Request $request, AbobaRepository $aboba, int $id): Response
+    {
+        $currentAboba = $aboba->getOne($id);
+        return !$currentAboba ? throw $this->createNotFoundException('Aboba not found') : $this->json($currentAboba);
+    }
+
+    #[Route('aboba/{id<\d+>}', name: 'app_aboba_page', methods: ['GET'])]
+    public function abobaPage(Request $request, AbobaRepository $aboba, int $id): Response
+    {
+        $currentAboba = $aboba->getOne($id);
+        if (!$currentAboba) {
+            throw $this->createNotFoundException('Aboba not found');
+        }
+
+        return $this->render('app/aboba_page.html.twig', ['aboba' => $currentAboba, 'id' => $id]);
     }
 }
+
