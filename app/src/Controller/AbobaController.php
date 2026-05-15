d38879b90ae8e58dd2a3b2a2f257d3b134d2fb89
AleksandrB <alexbyk36@gmail.com>
2026-05-15 11:05:51 +0500

    feat: using slugs instead of ids in AbobaController.php

--- app/src/Controller/AbobaController.php
+++ app/src/Controller/AbobaController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use App\Entity\Aboba;
 use App\Repository\AbobaRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AbobaController extends AbstractController
 {
-    #[Route('aboba/{aboba}', name: 'app_aboba_page', methods: ['GET'])]
-    public function abobaPage(Aboba $aboba) : Response
-    {
+    #[Route('aboba/{slug}', name: 'app_aboba_page', methods: ['GET'])]
+    public function abobaPage(
+        #[MapEntity(mapping: ['slug' => 'slug'])]
+        Aboba $aboba
+    ): Response {
         return $this->render('app/abobaPage.html.twig', ['aboba' => $aboba]);
     }
 }
